1f71d34f0f21892726e3c2268396592ceab33c9c
Caio Maia <caio.maiasb@gmail.com>
2025-10-26 15:44:00 -0300

    adição de deploy no aws

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,21 @@
+import './index.css'
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <div className='sectionBar'>
+        <h1>Lenita Brasil</h1>
+      </div>
+
+      <div className='sectionBar'>
+        <h1>Navegação</h1>
+      </div>
+
+      <div className='sectionBar'>
+        <h1>Pesquisar</h1>
+      </div>
+    </div>
+  )
+}
+
+export { NavBar }

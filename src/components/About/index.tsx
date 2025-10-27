928703dd88db4af98283c04af975f111e5d5fbec
Caio Maia <caio.maiasb@gmail.com>
2025-10-27 16:56:18 -0300

    Ajuste de texto do componente 'SOBRE'

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,23 +1,20 @@
 import './index.css'
-import avatar from '../../assets/avatar.png'
+// import avatar from '../../assets/avatar.png'
 
 function About() {
   return (
     <div id='sobre' className='about'>
-      <div className='about-image'>
-        <img src={avatar} width={650}/>
-      </div>
-      <div className='about-text'>
-        <h1>Quem é Lenita Brasil?</h1>
-        <label>
-          A Lenita Brasil é referência em rejuvenescimento e
-          cuidados integrados, unindo excelência técnica,
-          atendimento humanizado e estrutura premium.
-          Oferecendo agilidade, conforto e resultados seguros
-          para pacientes que valorizam qualidade, exclusividade
-          e conﬁança.
-        </label>
-      </div>
+        <div className='sobre-content'>
+          <h1>Quem é Lenita Brasil?</h1>
+          <label>
+            A Lenita Brasil é referência em rejuvenescimento e
+            cuidados integrados, unindo excelência técnica,
+            atendimento humanizado e estrutura premium.
+            Oferecendo agilidade, conforto e resultados seguros
+            para pacientes que valorizam qualidade, exclusividade
+            e conﬁança.
+          </label>
+        </div>
     </div>
   )
 }
